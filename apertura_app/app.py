e7a13b73cb7527805f261630779cb50279f65ebd
Ángel Gerardo De La Mora Wolburg <132295875+wolburg@users.noreply.github.com>
2026-06-05 12:38:08 -0600

    Update app.py

--- apertura_app/app.py
+++ apertura_app/app.py
@@ -632,8 +632,8 @@ def _kyc_pm(sol):
         "D13": lambda s: "MORAL",
         "E20": lambda s: e.giro,
         "O3": lambda s: s.fecha_alta, 
-        "C69": lambda s: rl.nombre_completo,
-        "N70": lambda s: rl.numero_identificacion,
+        "D69": lambda s: rl.nombre_completo,
+        "O70": lambda s: rl.numero_identificacion,
         "C79": lambda s: f"_______________________________\n\n{e.razon_social}",
     }}
 
